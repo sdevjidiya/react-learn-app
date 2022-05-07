5f273116f3be346d2170552ec2689474957dcf51
sdevjidiya <suresh.b.jidiya@gmail.com>
2022-05-07 19:06:27 +0530

    template convert

---
+++ blog-app/src/App.js
@@ -0,0 +1,19 @@
+import NavBar from "./Componets/NavBar";
+import Header from "./Componets/Header";
+import Footer from "./Componets/Footer";
+import Home from "./Componets/Home";
+import About from "./Componets/About";
+
+function App() {
+  return (
+    <>
+      <NavBar/>
+      <Header/>
+      <Home/>
+      <About/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
